1cae0ab008fb56a0fb115b2e0a44dbd2ee618f2e
moloch-kim <79761359+moloch-kim@users.noreply.github.com>
2024-10-21 12:26:35 +0900

    Q1 : Implementation 1
    스켈레톤 코드를 받아왔으며, 그 코드에 이미 구현사항이 요구하는것을 포함하고있음

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+
+public class QuestManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // [구현사항 1] 정적 필드 정의
+    private static QuestManager instance;
+
+    // [구현사항 2] 정적 프로퍼티 정의
+    public static QuestManager Instance
     {
         
+        get
+        {
+            return instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // [구현사항 3] 인스턴스 검사 로직
+    private void Awake()
     {
-        
+        instance = this;
     }
 }
