f53c4cea900be7e4c31706969f38de4355ecd747
moloch-kim <79761359+moloch-kim@users.noreply.github.com>
2024-10-22 19:48:07 +0900

    Q2 Implmentation 2
    오브젝트 풀들을 키별로 관리하기 위해 오브젝트 풀 매니저라는 상위 클래스를 만들어 딕셔너리로 string키값으로 풀들을 관리하도록 하였습니다.

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -6,7 +6,7 @@ public class ObjectPool : MonoBehaviour
 {
     public GameObject prefab;
     private List<GameObject> pool = new List<GameObject>();
-    public int poolSize = 300;
+    public int poolSize;
 
     void Start()
     {
@@ -33,5 +33,34 @@ public class ObjectPool : MonoBehaviour
     public void Release(GameObject obj)
     {
         obj.SetActive(false);
+    }
+}
+
+public class ObjectPoolManager : MonoBehaviour
+{
+    private Dictionary<string, ObjectPool> pools = new Dictionary<string, ObjectPool>();
+
+    public GameObject Get(string key)
+    {
+        if (pools.ContainsKey(key))
+        {
+            return pools[key].Get();
+        }
+        else
+        {
+            Debug.LogError("해당 키의 오브젝트 풀이 존재하지 않습니다: " + key);
+            return null;
+        }
+    }
+    public void Release(string key, GameObject obj)
+    {
+        if (pools.ContainsKey(key))
+        {
+            pools[key].Release(obj);
+        }
+        else
+        {
+            Debug.LogError("해당 키의 오브젝트 풀이 존재하지 않습니다: " + key);
+        }
     }
 }
